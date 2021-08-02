4f25fd7af38b1bfa399ef7c659113a50d32b684f
macklark <ganesh.kolavennu@gmail.com>
2021-08-02 13:42:30 +0530

    added filter capabilties but not working will fix

--- pages/events/[...slug].jsx
+++ pages/events/[...slug].jsx
@@ -1,4 +1,50 @@
+import { useRouter } from "next/router";
+
 export default function Filtered() {
+  const router = useRouter();
+
+  const events = [
+    {
+      id: 1,
+      title: "React conf",
+      image: "../public/react.jpg",
+      date: "2021-04-21",
+    },
+    {
+      id: 2,
+      title: "Vue conf",
+      image: "../public/vue.jpg",
+      date: "2022-02-22",
+    },
+    {
+      id: 3,
+      title: "Angular conf",
+      image: "../public/angular.jpg",
+      date: "2021-01-23",
+    },
+  ];
+
+  const filterer = () => {
+    const nYear = +router.query.slug[0];
+    const nMonth = +router.query.slug[1];
+
+    const filter = events.filter((event) => {
+      const eventDate = new Date(event.date);
+      console.log(eventDate);
+      if (!router.query.slug) {
+        return <p className="text-center">Loading...</p>;
+      }
+
+      return (
+        eventDate.getFullYear() === nYear && eventDate.getMonth() === nMonth
+      );
+    });
+
+    return filter;
+  };
+
+  filterer();
+
   return (
     <div className="mt-10 text-center">
       <h1>Filtered Pages</h1>
--- pages/events/index.jsx
+++ pages/events/index.jsx
@@ -1,16 +1,41 @@
 import Navbar from "../../components/Navbar";
 import Link from "next/link";
+import Search from "../../components/Search";
+import { useRouter } from "next/router";
 
 export default function Events() {
+  const router = useRouter();
   const events = [
-    { id: 1, title: "React conf", image: "../public/react.jpg" },
-    { id: 2, title: "Vue conf", image: "../public/vue.jpg" },
-    { id: 3, title: "Angular conf", image: "../public/angular.jpg" },
+    {
+      id: 1,
+      title: "React conf",
+      image: "../public/react.jpg",
+      date: "2021-04-21",
+    },
+    {
+      id: 2,
+      title: "Vue conf",
+      image: "../public/vue.jpg",
+      date: "2022-02-22",
+    },
+    {
+      id: 3,
+      title: "Angular conf",
+      image: "../public/angular.jpg",
+      date: "2021-01-23",
+    },
   ];
+
+  const finder = (year, month) => {
+    const path = `/events/${year}/${month}`;
+
+    router.push(path);
+  };
 
   return (
     <div className="container mx-auto mt-20">
       <Navbar />
+      <Search onSearch={finder} />
       <h1 className="text-center mt-10 text-3xl">Events page</h1>
       {events.map((event) => {
         return (
