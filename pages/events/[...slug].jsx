d95bcb3fbdd534f0f1713f905b7ad3cf56e75e47
macklark <ganesh.kolavennu@gmail.com>
2021-08-04 09:55:37 +0530

    updated and fixed filter feature

--- pages/events/[...slug].jsx
+++ pages/events/[...slug].jsx
@@ -1,4 +1,5 @@
 import { useRouter } from "next/router";
+import Link from "next/link";
 
 export default function Filtered() {
   const router = useRouter();
@@ -8,46 +9,66 @@ export default function Filtered() {
       id: 1,
       title: "React conf",
       image: "../public/react.jpg",
-      date: "2021-04-21",
+      date: "2022-05-21",
     },
     {
       id: 2,
       title: "Vue conf",
       image: "../public/vue.jpg",
-      date: "2022-02-22",
+      date: "2022-05-02",
     },
     {
       id: 3,
       title: "Angular conf",
       image: "../public/angular.jpg",
-      date: "2021-01-23",
+      date: "2021-04-23",
     },
   ];
 
   const filterer = () => {
-    const nYear = +router.query.slug[0];
-    const nMonth = +router.query.slug[1];
+    const filterData = router.query.slug;
+    const filterYear = filterData[0];
+    const filterMonth = filterData[1];
+    const nYear = +filterYear;
+    const nMonth = +filterMonth;
 
     const filter = events.filter((event) => {
       const eventDate = new Date(event.date);
-      console.log(eventDate);
       if (!router.query.slug) {
         return <p className="text-center">Loading...</p>;
       }
 
       return (
-        eventDate.getFullYear() === nYear && eventDate.getMonth() === nMonth
+        eventDate.getFullYear() === nYear && eventDate.getMonth() + 1 === nMonth
       );
     });
 
     return filter;
   };
 
-  filterer();
+  if (router.query.slug == undefined) {
+    return <p className="text-center">Loading...</p>;
+  } else {
+    filterer();
+  }
 
   return (
     <div className="mt-10 text-center">
-      <h1>Filtered Pages</h1>
+      {filterer().length == 0 && (
+        <p className="text-2xl text-center">No events on this date</p>
+      )}
+      {filterer().map((event) => {
+        return (
+          <div
+            key={event.id}
+            className="flex justify-center content-center mb-10"
+          >
+            <Link href={`/events/${event.title}`}>
+              <a className="text-4xl">{event.title}</a>
+            </Link>
+          </div>
+        );
+      })}
     </div>
   );
 }
